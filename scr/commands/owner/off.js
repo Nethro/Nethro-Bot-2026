02c3e78b2484980c0a66ad2a44e30cde3472c927
Nethro <164420931+Nethro@users.noreply.github.com>
2025-07-31 10:28:26 -0500

    Update off.js

--- scr/commands/owner/off.js
+++ scr/commands/owner/off.js
@@ -1,22 +1,17 @@
 const { PREFIX } = require(`${BASE_DIR}/config`);
 const { deactivateGroup } = require(`${BASE_DIR}/utils/database`);
+const { WarningError } = require(`${BASE_DIR}/errors`);
 
 module.exports = {
   name: "off",
-  description: "Desactiva el bot en el grupo",
+  description: "🔇 Desactiva al Nethro-Bot en el grupo. Se va de vacaciones... por ahora 😌",
   commands: ["off"],
   usage: `${PREFIX}off`,
+
   /**
    * @param {CommandHandleProps} props
    * @returns {Promise<void>}
    */
   handle: async ({ sendSuccessReply, remoteJid, isGroup }) => {
     if (!isGroup) {
-      throw new WarningError("Este comando debe usarse dentro de un grupo.");
-    }
-
-    deactivateGroup(remoteJid);
-
-    await sendSuccessReply("¡Bot desactivado en el grupo!");
-  },
-};
+      throw new WarningEr
